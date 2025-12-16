a4aef0eeab14a41eb09813d4e190a213cedcf7d8
John Yk <yikok.yong@gmail.com>
2025-12-17 04:49:50 +0800

    feat: implement shake detection and event emission in ReactNativeShake module

--- ios/ReactNativeShake.h
+++ ios/ReactNativeShake.h
@@ -1,5 +1,9 @@
 #import <ReactNativeShakeSpec/ReactNativeShakeSpec.h>
+#import <React/RCTEventEmitter.h>
 
-@interface ReactNativeShake : NSObject <NativeReactNativeShakeSpec>
+@interface ReactNativeShake : RCTEventEmitter <NativeReactNativeShakeSpec>
+
+@property (nonatomic, assign) BOOL isListening;
+@property (nonatomic, assign) BOOL hasListeners;
 
 @end
--- ios/ReactNativeShake.mm
+++ ios/ReactNativeShake.mm
@@ -1,21 +1,119 @@
 #import "ReactNativeShake.h"
+#import <React/RCTBridge.h>
+#import <UIKit/UIKit.h>
+
+@interface ShakeWindow : UIWindow
+@property(nonatomic, weak) ReactNativeShake *shakeModule;
+@end
+
+@implementation ShakeWindow
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+  if (motion == UIEventSubtypeMotionShake && self.shakeModule.isListening) {
+    [self.shakeModule sendShakeEvent];
+  }
+  [super motionEnded:motion withEvent:event];
+}
+
+@end
 
 @implementation ReactNativeShake
-- (NSNumber *)multiply:(double)a b:(double)b {
-    NSNumber *result = @(a * b);
 
-    return result;
+RCT_EXPORT_MODULE()
+
+- (instancetype)init {
+  if (self = [super init]) {
+    _isListening = NO;
+    _hasListeners = NO;
+  }
+  return self;
+}
+
+- (NSArray<NSString *> *)supportedEvents {
+  return @[ @"ShakeEvent" ];
+}
+
+- (void)startObserving {
+  _hasListeners = YES;
+}
+
+- (void)stopObserving {
+  _hasListeners = NO;
+}
+
+- (void)startShakeDetection {
+  if (_isListening) {
+    return;
+  }
+
+  _isListening = YES;
+
+  dispatch_async(dispatch_get_main_queue(), ^{
+    UIWindow *window = [self getKeyWindow];
+    if (window && ![window isKindOfClass:[ShakeWindow class]]) {
+      ShakeWindow *shakeWindow =
+          [[ShakeWindow alloc] initWithFrame:window.frame];
+      shakeWindow.shakeModule = self;
+      shakeWindow.rootViewController = window.rootViewController;
+      shakeWindow.windowLevel = window.windowLevel;
+      [shakeWindow makeKeyAndVisible];
+    } else if ([window isKindOfClass:[ShakeWindow class]]) {
+      ((ShakeWindow *)window).shakeModule = self;
+    }
+  });
+}
+
+- (void)stopShakeDetection {
+  if (!_isListening) {
+    return;
+  }
+
+  _isListening = NO;
+
+  dispatch_async(dispatch_get_main_queue(), ^{
+    UIWindow *window = [self getKeyWindow];
+    if ([window isKindOfClass:[ShakeWindow class]]) {
+      ((ShakeWindow *)window).shakeModule = nil;
+    }
+  });
+}
+
+- (void)sendShakeEvent {
+  if (_hasListeners) {
+    [self sendEventWithName:@"ShakeEvent" body:@{}];
+  }
+}
+
+- (UIWindow *)getKeyWindow {
+  NSArray<UIWindow *> *windows = [[UIApplication sharedApplication] windows];
+  for (UIWindow *window in windows) {
+    if (window.isKeyWindow) {
+      return window;
+    }
+  }
+  return nil;
+}
+
+- (void)addListener:(NSString *)eventType {
+  // Required for RCTEventEmitter
+}
+
+- (void)removeListeners:(double)count {
+  // Required for RCTEventEmitter
 }
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
-{
-    return std::make_shared<facebook::react::NativeReactNativeShakeSpecJSI>(params);
+    (const facebook::react::ObjCTurboModule::InitParams &)params {
+  return std::make_shared<facebook::react::NativeReactNativeShakeSpecJSI>(
+      params);
 }
 
-+ (NSString *)moduleName
-{
++ (NSString *)moduleName {
   return @"ReactNativeShake";
+}
+
++ (BOOL)requiresMainQueueSetup {
+  return YES;
 }
 
 @end
