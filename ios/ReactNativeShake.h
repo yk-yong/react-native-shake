fbaf39e5b5291f2d0da6392a82a46dd71a94c7ac
John Yk <yikok.yong@gmail.com>
2025-12-19 08:08:57 +0800

    fix: reorder import statement and adjust property formatting for consistency

--- ios/ReactNativeShake.h
+++ ios/ReactNativeShake.h
@@ -1,9 +1,11 @@
-#import <ReactNativeShakeSpec/ReactNativeShakeSpec.h>
 #import <React/RCTEventEmitter.h>
+#import <ReactNativeShakeSpec/ReactNativeShakeSpec.h>
 
 @interface ReactNativeShake : RCTEventEmitter <NativeReactNativeShakeSpec>
 
-@property (nonatomic, assign) BOOL isListening;
-@property (nonatomic, assign) BOOL hasListeners;
+@property(nonatomic, assign) BOOL isListening;
+@property(nonatomic, assign) BOOL hasListeners;
+
+- (void)sendShakeEvent;
 
 @end
